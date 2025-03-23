307760e652b2c756d44adc8371d93ca7459448df
javier <javiersneiderrm@ufps.edu.co>
2025-03-22 23:43:27 -0500

    dejando que pida datos para la matriz

--- src/multiplicacionmatricesbinarias/Modelo/SistemaMatrizBinaria.java
+++ src/multiplicacionmatricesbinarias/Modelo/SistemaMatrizBinaria.java
@@ -12,56 +12,48 @@ public class SistemaMatrizBinaria {
     public SistemaMatrizBinaria() {
     }
 
-public boolean[][] getMultiplicacion(boolean m1[][], boolean m2[][]) {
-    boolean[][] matriz = new boolean[m1.length][m2[0].length];
-
-    // Verificación de dimensiones (opcional, pero recomendado)
-    if (m1[0].length != m2.length) {
-        throw new IllegalArgumentException("Las dimensiones de las matrices no son compatibles para la multiplicación");
-    }
-
-    int contador = 0;
-    int m = 0;
-    int s = 1;
-    for (int i = 0; i <= m1.length - 1; i++) {
-        int j = m2.length - 1;
-
-        boolean suma = false;
-        for (int n = m1[0].length - 1; m <= m2[0].length - 1; n--) {
-            contador++;
-            boolean a1 = m1[i][n];
-            boolean b1 = m2[j][m];
-
-            // Realizar la operación AND
-            boolean producto = a1 && b1;
-
-            // Acumular el resultado usando la operación OR (suma booleana)
-            if (s > 0) {
-                suma = producto; // Inicializar la suma con el primer producto
-                s--;
-            } else {
-                if(suma == true && producto == true){
-                    suma = false;  
-                }else{
-                    suma = suma || producto; // Acumular con OR
-                }
-                
-            }
-
-            j--;
-            if (j == -1) {
-                matriz[i][m] = suma; // Asignar el resultado acumulado
-                n = m1[0].length; // Reiniciar el índice de columnas de m1
-                m++; // Mover a la siguiente columna de m2
-                j = m2.length - 1; // Reiniciar el índice de filas de m2
-                s = 1; // Reiniciar el contador de suma
-            }
+    public boolean[][] getMultiplicacion(boolean m1[][], boolean m2[][]) {
+        boolean[][] matriz = new boolean[m1.length][m2[0].length];
+        if (m1[0].length != m2.length) {
+            throw new IllegalArgumentException("Las dimensiones de las matrices no son compatibles para la multiplicación");
         }
-        m = 0; // Reiniciar el índice de columnas de m2 para la siguiente fila de m1
-    }
 
-    return matriz;
-}
+        int contador = 0;
+        int m = 0;
+        int s = 1;
+        for (int i = 0; i <= m1.length - 1; i++) {
+            int j = m2.length - 1;
+
+            boolean suma = false;
+            for (int n = m1[0].length - 1; m <= m2[0].length - 1; n--) {
+                contador++;
+                boolean a1 = m1[i][n];
+                boolean b1 = m2[j][m];
+
+                boolean producto = a1 && b1;
+
+                if (s > 0) {
+                    suma = producto; 
+                    s--;
+                } else {
+
+                    suma = suma ^ producto; 
+                }
+
+                j--;
+                if (j == -1) {
+                    matriz[i][m] = suma; 
+                    n = m1[0].length;
+                    m++; 
+                    j = m2.length - 1;
+                    s = 1; 
+                }
+            }
+            m = 0; 
+        }
+
+        return matriz;
+    }
 
     public boolean[][] getM1() {
         return m1;
--- src/multiplicacionmatricesbinarias/Vista/ImprimirMatriz.java
+++ src/multiplicacionmatricesbinarias/Vista/ImprimirMatriz.java
@@ -1,20 +1,14 @@
-
-
 package multiplicacionmatricesbinarias.Vista;
 
 import multiplicacionmatricesbinarias.Controller.Consola;
 import multiplicacionmatricesbinarias.Modelo.SistemaMatrizBinaria;
-
-
 
 /**
  *
  * @author nn
  */
 public class ImprimirMatriz {
-    
-    
-    
+
     /**
      * @param args the command line arguments
      */
@@ -22,66 +16,54 @@ public class ImprimirMatriz {
         // TODO code application logic here
         SistemaMatrizBinaria model = new SistemaMatrizBinaria();
         Consola c = new Consola();
-        boolean n = true && true ;
-        System.out.println(n);
-        
-        byte filas = 5;//c.leerByte("Numero de filas de Matriz 1: ");
-        byte columnas = 4; // c.leerByte("Numero de Columnasde Matriz 1: ");
+
+        byte filas = c.leerByte("Numero de filas de Matriz 1: ");
+        byte columnas = c.leerByte("Numero de Columnasde Matriz 1: ");
         boolean[][] m1 = generarMatrizEnteros(filas, columnas);
-               
-        byte filas2 = 4;//c.leerByte("Numero de filas de Matriz 2: ");
-        byte columnas2 = 3; //c.leerByte("Numero de Columnasde Matriz 2: ");
+
+        byte filas2 = c.leerByte("Numero de filas de Matriz 2: ");
+        byte columnas2 = c.leerByte("Numero de Columnasde Matriz 2: ");
         boolean[][] m2 = generarMatrizEnteros(filas2, columnas2);
-        
-        if(columnas != filas2){
-            c.imprimir("No se puede columnas de M1 debe ser igual a filas de M2");
-        }else{
-            
-              // Imprimir la matriz
+
         System.out.println("Matriz generada1:");
         for (int i = 0; i < filas; i++) {
             for (int j = 0; j < columnas; j++) {
-                System.out.print(m1[i][j] ? "1 " : "0 "); // Mostrar 1 para true y 0 para false
+                System.out.print(m1[i][j] ? "1 " : "0 ");
             }
             System.out.println();
         }
-           
-           // Imprimir la matriz
+
         System.out.println("Matriz generada2:");
         for (int i = 0; i < filas2; i++) {
             for (int j = 0; j < columnas2; j++) {
-                System.out.print(m2[i][j] ? "1 " : "0 "); // Mostrar 1 para true y 0 para false
+                System.out.print(m2[i][j] ? "1 " : "0 "); 
             }
             System.out.println();
-        }
-        
-        boolean [][] mr = model.getMultiplicacion(m1,m2);
-         System.out.println("Final:");
-        for (int i = 0; i <= mr.length-1; i++) {
-            for (int j = 0; j <= mr[0].length-1; j++) {
-                System.out.print(mr[i][j] ? "1 " : "0 "); // Mostrar 1 para true y 0 para false
-            }
-            System.out.println();
-        }
-        
         }
 
-        
-     
+        boolean[][] mr = model.getMultiplicacion(m1, m2);
+        System.out.println("Resultado:");
+        for (int i = 0; i <= mr.length - 1; i++) {
+            for (int j = 0; j <= mr[0].length - 1; j++) {
+                System.out.print(mr[i][j] ? "1 " : "0 "); 
+            }
+            System.out.println();
+        }
+
     }
-    
+
     public static boolean[][] generarMatrizEnteros(byte filas, byte columnas) {
-        
+
         boolean[][] matriz = new boolean[filas][columnas];
-        
+
         for (int i = 0; i < filas; i++) {
-            int valor = i; 
+            int valor = i;
             for (int j = columnas - 1; j >= 0; j--) {
                 matriz[i][j] = (valor & 1) == 1;
                 valor = valor >> 1;
             }
         }
-        
+
         return matriz;
     }
 }
